3749c1a49dc22c73f695d2609e558279a3cfe5ad
alemonacoo <ale.monaco@outlook.com>
2022-09-20 15:04:59 +0200

    setting up main files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log("JS OK!");
+
+// const app = new Vue({
+//     el: "#app",
+//     data: {
+//         emails: [],
+//     },
+//     mounted(){
+//         const self = this;
+
+//         axios.get(https://flynn.boolean.careers/exercises/api/random/mail)
+
+
+//     }
+
+
+// })
