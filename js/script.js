6c4eb225d0518e02ba13299d19a69034bacf6caa
alemonacoo <ale.monaco@outlook.com>
2022-09-20 15:17:01 +0200

    Excercise completed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 console.log("JS OK!");
 
-// const app = new Vue({
-//     el: "#app",
-//     data: {
-//         emails: [],
-//     },
-//     mounted(){
-//         const self = this;
-
-//         axios.get(https://flynn.boolean.careers/exercises/api/random/mail)
-
-
-//     }
-
-
-// })
+const app = new Vue({
+    el: "#app",
+    data: {
+        emails: [],
+        emailsLength: 10
+    },
+    mounted(){
+        for(i=0; i<=this.emailsLength; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+            this.emails.push(response.data.response);
+            console.log('email is: ' + response.data.response);
+          })
+          .catch((error) => {
+            console.warn(error);
+            this.text = 'Sorry, unable to retrieve this email';
+          });
+        }
+        console.log(this.emails);
+    }
+})
