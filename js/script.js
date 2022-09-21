4dedd6edf03adb93f2da773967cec3994f8019f5
alemonacoo <ale.monaco@outlook.com>
2022-09-21 12:44:46 +0200

    emails appear at the same tie

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 console.log("JS OK!");
 
+const EMAILS_LENGTH = 10;
+
 const app = new Vue({
     el: "#app",
     data: {
         emails: [],
-        emailsLength: 10
+        EMAILS_LENGTH,
+        loading: false
     },
     mounted(){
-        for(i=0; i<=this.emailsLength; i++){
+        for(i=0; i<this.EMAILS_LENGTH; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+            if(response.status === 200){
             this.emails.push(response.data.response);
             console.log('email is: ' + response.data.response);
+            if(this.emails.length == EMAILS_LENGTH){
+                this.loading = true;
+            }
+        }
           })
           .catch((error) => {
             console.warn(error);
-            this.text = 'Sorry, unable to retrieve this email';
           });
         }
-        console.log(this.emails);
     }
 })
